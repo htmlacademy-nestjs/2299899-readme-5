acae829bb3704153723d6e1415ab5a7dd08ade9e
JustDoIt <vadim.v.antipov@gmail.com>
2023-12-13 13:36:34 +0300

    feat: shared interfaces for user module

---
+++ project/shared-libs/types/src/lib/user.interface.ts
@@ -0,0 +1,10 @@
+import { UserRole } from './user-role.enum';
+
+export interface User {
+  id?: string;
+  email: string;
+  username: string;
+  name:string;
+  birthDate: Date;
+  role: UserRole;
+}
