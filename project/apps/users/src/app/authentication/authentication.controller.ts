c1726ed82bf28426b3a0619b56aefb27d34bf0f5
JustDoIt <vadim.v.antipov@gmail.com>
2023-12-13 16:37:04 +0300

    feat: open api scpecification for authentication

--- project/apps/users/src/app/authentication/authentication.controller.ts
+++ project/apps/users/src/app/authentication/authentication.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, Post } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { fillDto } from '@project/helpers';
 
 import { AuthneticationService } from './authentication.service';
@@ -7,24 +8,43 @@ import { LoginUserDto } from './dto/login-user.dto';
 import { LoggedUserRdo } from './rdo/logged-user.rdo';
 import { UserRdo } from './rdo/user.rdo';
 
+@ApiTags('authentication')
 @Controller('auth')
 export class AuthenticationController {
   constructor(
     private readonly authService: AuthneticationService,
   ) {}
 
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'New user has been successfully created.'
+  })
   @Post('register')
   public async create(@Body() dto: CreateUserDto) {
     const newUser = await this.authService.register(dto);
     return fillDto(UserRdo, newUser.toPOJO());
   }
 
+  @ApiResponse({
+    type: LoggedUserRdo,
+    status: HttpStatus.OK,
+    description: 'User has been successfully logged.'
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Wrong password or login.'
+  })
   @Post('login')
   public async login(@Body() dto: LoginUserDto) {
     const verifiedUser = await this.authService.verifyUser(dto);
     return fillDto(LoggedUserRdo, verifiedUser.toPOJO());
   }
 
+  @ApiResponse({
+    type: UserRdo,
+    status: HttpStatus.OK,
+    description: 'User found.'
+  })
   @Get(':id')
   public async show(@Param('id') id: string) {
     const existUser = await this.authService.getUser(id);
--- project/apps/users/src/app/authentication/dto/create-user.dto.ts
+++ project/apps/users/src/app/authentication/dto/create-user.dto.ts
@@ -1,7 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'Unique user\'s email.',
+    example: 'user@user.com',
+  })
   email: string;
+
+  @ApiProperty({
+    description: 'User\'s login/username.',
+    example: 'keks',
+  })
   username: string;
+
+  @ApiProperty({
+    description: 'User\'s firstname and lastname.',
+    example: 'John Doe',
+  })
   name: string;
+
+  @ApiProperty({
+    description: 'User\'s date of birth.',
+    example: '2002-02-02',
+  })
   birthDate: string;
+
+  @ApiProperty({
+    description: 'User\'s password.',
+    example: '123456',
+  })
   password: string;
 }
