8d93723f6be9f0bd4bee3f3f00b23f0b728aed24
JustDoIt <vadim.v.antipov@gmail.com>
2024-01-07 12:57:32 +0300

    feat: updated postgres prisma scheme for post, comment, type, favorites

---
+++ project/shared-libs/blog/models/src/lib/shared-libs-blog-models.spec.ts
@@ -0,0 +1,7 @@
+import { sharedLibsBlogModels } from './shared-libs-blog-models';
+
+describe('sharedLibsBlogModels', () => {
+  it('should work', () => {
+    expect(sharedLibsBlogModels()).toEqual('shared-libs-blog-models');
+  });
+});
---
+++ project/shared-libs/blog/models/src/lib/shared-libs-blog-models.ts
@@ -0,0 +1,3 @@
+export function sharedLibsBlogModels(): string {
+  return 'shared-libs-blog-models';
+}
