5e72bb467e9e3336c6af0d03f9fbd3a103c70926
JustDoIt <vadim.v.antipov@gmail.com>
2024-01-12 14:42:34 +0300

    fix: not imported config for file-storage

--- project/shared-libs/config/file-storage/src/lib/file-storage-config.module.ts
+++ project/shared-libs/config/file-storage/src/lib/file-storage-config.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import fileStorageConfig from './file-storage.config';
+
 const ENV_FILE_PATH = 'apps/file-storage/file-storage.env';
 
 @Module({
@@ -8,7 +10,7 @@ const ENV_FILE_PATH = 'apps/file-storage/file-storage.env';
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
-      load: [],
+      load: [fileStorageConfig],
       envFilePath: ENV_FILE_PATH,
     }),
   ]
