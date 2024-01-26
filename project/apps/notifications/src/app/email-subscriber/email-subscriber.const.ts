900004d797df9f466afbb141962cc77fcd9b9f6b
JustDoIt <vadim.v.antipov@gmail.com>
2024-01-26 18:25:41 +0300

    refactor: some notifications consts

--- project/apps/notifications/src/app/email-subscriber/email-subscriber.const.ts
+++ project/apps/notifications/src/app/email-subscriber/email-subscriber.const.ts
@@ -1,3 +1,4 @@
-export const EMAIL_NOT_VALID = 'Email is not valid';
-export const NAME_IS_EMPTY = 'Name is empty';
-export const USER_ID_IS_EMPTY = 'User id is empty';
+export enum ValidationMessage {
+  Email = 'Email is not valid',
+  Name = 'name is empty',
+}
--- project/apps/notifications/src/main.ts
+++ project/apps/notifications/src/main.ts
@@ -12,6 +12,7 @@ import { GLOBAL_PREFIX } from './const';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(GLOBAL_PREFIX);
+
   const port = process.env.APP_PORT;
   await app.listen(port);
   Logger.log(`🚀 Application "Notifications" is running on: http://localhost:${port}/${GLOBAL_PREFIX}`);
